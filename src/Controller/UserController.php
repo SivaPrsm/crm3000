d8f54eced4bc2410bedf021c0dd465f54db78912
Siva Parasuraman <para-siva@hotmail.fr>
2021-10-01 16:31:20 +0200

    feat: controller methods for user management

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,16 +5,39 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
 
 class UserController extends AbstractController
 {
+	/*
+	 * @UserRepository
+	 */
+	private $repository;
+
+	public function __construct(UserRepository $repository)
+	{
+		$this->repository = $repository;	
+	}
+
     /**
      * @Route("/user", name="user")
      */
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
-    }
+		$users = $this->repository->findAll();
+		return $this->render('user/index.html.twig', [
+			'users' => $users,
+		]);
+	}
+
+	/**
+	 * @Route("/user/edit", name="user.edit")
+	 */
+	public function edit(User $user)
+	{
+		return $this->render('user/edit.html.twig', [
+			'user' => $user,
+		]);
+	}
+
 }
